b1d7b34953ca54883ce95369b945a51138416fb6
dev68sft <dev68sft@gmail.com>
2023-11-20 16:50:07 +0200

    commit #1

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,23 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+
+    public static void main(String[] args)  {
 
         Scanner con = new Scanner(System.in);
         int num_lab;
         System.out.println("Лабораторні роботи");
         System.out.print("Введіть номер роботи (1-3):");
-        num_lab = con.nextInt();
-
-        System.out.println("Вибрано лабораторну роботу №: " + num_lab);
+        try {
+            num_lab = con.nextInt();
+            if ((1 <= num_lab) && (num_lab <= 3)) {
+                System.out.println("Вибрано лабораторну роботу №: " + num_lab);
+            } else {
+                throw new Exception("Номер лабораторної роботи має бути від 1 до 3");
+            }
+        } catch (Exception e) {
+            System.out.println("Помилка вводу номеру лабораторної роботи");
+            System.out.println(e.getMessage());
+        }
     }
 }
