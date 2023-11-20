68ae5bd4ab4596d57ccd3b7664db4ef5e10249fd
dev68sft <dev68sft@gmail.com>
2023-11-20 16:10:58 +0200

    commit #1

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,17 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        Scanner con = new Scanner(System.in);
+        int num_lab;
+        System.out.println("Лабораторні роботи");
+        System.out.print("Введіть номер роботи (1-3):");
+        num_lab = con.nextInt();
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("I = " + i);
-        }
+        System.out.println("Вибрано лабораторну роботу №: " + num_lab);
     }
 }
